013430f096e69b9caf5929bb77d5c0585e1c2af6
facundolamote1988 <faculamote@gmail.com>
2023-06-20 00:57:43 -0300

    arreglos

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
-
-function confirmar(event) {
+//FUNCION QUE CONFIRMA ACCESO Y PASA AL GESTOR DE TAREAS. (captura los datos x consola)
+function confirmar(event) { //EVENT.PREVENT,NO DEJA ACCESDER SIN TODOS LOS CAMPOS COMPLETADOS.
     var nombre = document.getElementById("nombre completo").value;
     var apellido = document.getElementById("apellido").value;
     var email = document.getElementById("email").value;
--- tareas.ts
+++ tareas.ts
@@ -1,16 +1,23 @@
 // Creo las variables
-var lista = document.getElementById("lista");
-var nuevaTarea = document.getElementById("nueva");
+let lista: HTMLInputElement = <HTMLInputElement>document.getElementById("lista");
+let nuevaTarea: HTMLInputElement = <HTMLInputElement>document.getElementById("nueva");
+
+
 // Llamo a la funcion
 nuevaTarea.addEventListener("onclick", agregar);
+
 // FUNCIONES
 function agregar() {
-    var nuevaLista = nuevaTarea.value;
-    var newElement = document.createElement("li");
-    newElement.textContent = "".concat(nuevaLista);
+    const nuevaLista = nuevaTarea.value
+    const newElement = document.createElement("li");
+    newElement.textContent = `${nuevaLista}`;
     lista.appendChild(newElement);
     nuevaTarea.value = "";
+
 }
+
 //nuevaLista toma el valor ingresado, se crea un element "li"
 //ese nuevo elemento tiene el valor ingresado en "nuevaLista"
 //appendChild va agregando ese elemento dejando el campo en blanco para volver a ingresar otra tarea.
+
+
