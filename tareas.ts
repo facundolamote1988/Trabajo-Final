ef8e7c9f458ae9b3dba9a479d6e77ac137a1e64c
facundolamote1988 <faculamote@gmail.com>
2023-06-22 01:51:33 -0300

    comentarios para detalles en el codigo

--- tareas.ts
+++ tareas.ts
@@ -8,16 +8,15 @@ nuevaTarea.addEventListener("onclick", agregar);
 
 // FUNCIONES
 function agregar() {
-    const nuevaLista = nuevaTarea.value
+    const nuevaLista = nuevaTarea.value //nuevaLista toma el valor ingresado.
     if (nuevaLista != null && nuevaLista != "") {
-        const newElement = document.createElement("li");
-        newElement.textContent = `${nuevaLista}`;
+        const newElement = document.createElement("li");// Se crea un element "li"
+        newElement.textContent = `${nuevaLista}`;//Ese nuevo elemento tiene el valor ingresado en "nuevaLista"
         lista.appendChild(newElement);
-        nuevaTarea.value = "";
-    }
+        nuevaTarea.value = "";//appendChild va agregando ese elemento dejando el campo en blanco para volver a ingresar otra tarea.
+
+    } 
 }
-//nuevaLista toma el valor ingresado, se crea un element "li"
-//ese nuevo elemento tiene el valor ingresado en "nuevaLista"
-//appendChild va agregando ese elemento dejando el campo en blanco para volver a ingresar otra tarea.
+
 
 
